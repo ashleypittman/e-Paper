1d31609f6cd7e34cee1774ec0b4ebb250a902601
Ashley Pittman <ashley@pittman.co.uk>
2020-07-25 17:02:40 +0100

    Add modifications to 2in7 display as well.
    Signed-off-by: Ashley Pittman <ashley@pittman.co.uk>

--- RaspberryPi&JetsonNano/python/lib/waveshare_epd/epd2in7.py
+++ RaspberryPi&JetsonNano/python/lib/waveshare_epd/epd2in7.py
@@ -50,6 +50,7 @@ class EPD:
         self.GRAY2  = GRAY2
         self.GRAY3  = GRAY3 #gray
         self.GRAY4  = GRAY4 #Blackest
+        self.config = epdconfig.RaspberryPi()
 
     lut_vcom_dc = [0x00, 0x00,
         0x00, 0x08, 0x00, 0x00, 0x00, 0x02,
@@ -151,29 +152,29 @@ class EPD:
     
     # Hardware reset
     def reset(self):
-        epdconfig.digital_write(self.reset_pin, 1)
-        epdconfig.delay_ms(200) 
-        epdconfig.digital_write(self.reset_pin, 0)
-        epdconfig.delay_ms(10)
-        epdconfig.digital_write(self.reset_pin, 1)
-        epdconfig.delay_ms(200)   
+        self.config.digital_write(self.reset_pin, 1)
+        self.config.delay_ms(200)
+        self.config.digital_write(self.reset_pin, 0)
+        self.config.delay_ms(10)
+        self.config.digital_write(self.reset_pin, 1)
+        self.config.delay_ms(200)
 
     def send_command(self, command):
-        epdconfig.digital_write(self.dc_pin, 0)
-        epdconfig.digital_write(self.cs_pin, 0)
-        epdconfig.spi_writebyte([command])
-        epdconfig.digital_write(self.cs_pin, 1)
+        self.config.digital_write(self.dc_pin, 0)
+        self.config.digital_write(self.cs_pin, 0)
+        self.config.spi_writebyte([command])
+        self.config.digital_write(self.cs_pin, 1)
 
     def send_data(self, data):
-        epdconfig.digital_write(self.dc_pin, 1)
-        epdconfig.digital_write(self.cs_pin, 0)
-        epdconfig.spi_writebyte([data])
-        epdconfig.digital_write(self.cs_pin, 1)
+        self.config.digital_write(self.dc_pin, 1)
+        self.config.digital_write(self.cs_pin, 0)
+        self.config.spi_writebyte([data])
+        self.config.digital_write(self.cs_pin, 1)
         
     def ReadBusy(self):        
         logging.debug("e-Paper busy")
-        while(epdconfig.digital_read(self.busy_pin) == 0):      #  0: idle, 1: busy
-            epdconfig.delay_ms(200)                
+        while(self.config.digital_read(self.busy_pin) == 0):      #  0: idle, 1: busy
+            self.config.delay_ms(200)
         logging.debug("e-Paper busy release")
 
     def set_lut(self):
@@ -219,7 +220,7 @@ class EPD:
             self.send_data(self.gray_lut_ww[count])
     
     def init(self):
-        if (epdconfig.module_init() != 0):
+        if (self.config.module_init() != 0):
             return -1
             
         # EPD hardware init start
@@ -289,7 +290,7 @@ class EPD:
         return 0
 
     def Init_4Gray(self):
-        if (epdconfig.module_init() != 0):
+        if (self.config.module_init() != 0):
             return -1
         self.reset()
         
@@ -494,7 +495,7 @@ class EPD:
         
         self.gray_SetLut()
         self.send_command(0x12)
-        epdconfig.delay_ms(200)
+        self.config.delay_ms(200)
         self.ReadBusy()
         # pass
         
@@ -515,6 +516,7 @@ class EPD:
         self.send_command(0X07)
         self.send_data(0xA5)
         
-        epdconfig.module_exit()
+        self.config.module_exit()
+        self.config = None
 ### END OF FILE ###
 
